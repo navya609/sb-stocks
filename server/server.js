36936404c14ed42f6de38735d35bde2294d4780f
yaswanthhari <yaswanthharitaluru@gmail.com>
2026-07-05 21:43:27 +0530

    Switch to live MongoDB Atlas database

--- server/server.js
+++ server/server.js
@@ -23,25 +23,15 @@ app.get('/', (req, res) => {
   res.send('SB Stocks API is running');
 });
 
-const { MongoMemoryServer } = require('mongodb-memory-server');
-
+// Database connection
 const PORT = process.env.PORT || 5000;
 
-const connectDB = async () => {
-  try {
-    // Try to connect to the in-memory database to avoid local installation issues
-    console.log('Starting In-Memory MongoDB...');
-    const mongoServer = await MongoMemoryServer.create();
-    const mongoUri = mongoServer.getUri();
-    
-    await mongoose.connect(mongoUri);
-    console.log(`In-Memory MongoDB connected successfully at ${mongoUri}`);
-    
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Atlas connected successfully');
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  } catch (err) {
+  })
+  .catch((err) => {
     console.error('MongoDB connection error:', err);
     process.exit(1);
-  }
-};
-
-connectDB();
+  });
